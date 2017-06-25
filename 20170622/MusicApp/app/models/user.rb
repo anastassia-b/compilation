2f0c0e18ca71186c77b885d5b7f9831c9b705cb9
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-06-24 19:33:28 -0700

    Add User Authentication

--- 20170622/MusicApp/app/models/user.rb
+++ 20170622/MusicApp/app/models/user.rb
@@ -1,2 +1,52 @@
 class User < ApplicationRecord
+  validates( :email,
+    :password_digest,
+    :session_token,
+    :activation_token,
+    presence: true)
+  validates :password, length: { minimum: 6, allow_nil: true }
+  validates :activation_token, :email, :session_token, uniqueness: true
+
+  attr_reader :password
+  after_initialize :ensure_session_token
+  after_initialize :set_activation_token
+
+  has_many :notes
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    user && user.valid_password?(password) ? user : nil
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password).to_s
+  end
+
+  def valid_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = generate_unique_token_for_field(:session_token)
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= generate_unique_token_for_field(:session_token)
+  end
+
+  def set_activation_token
+    self.activation_token = generate_unique_token_for_field(:activation_token)
+  end
+
+  def activate!
+    self.update_attribute(:activated, true)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
 end
