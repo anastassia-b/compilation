d83e13988c98d64f897a1fd6d197f538d74ab3c9
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-06-24 22:45:29 -0700

    Add controllers

--- 20170622/MusicApp/app/controllers/users_controller.rb
+++ 20170622/MusicApp/app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def show
+    render :show
+  end
+
+  def create
+    @user = User.new(user_params)
+    
+  end
+
+  def activate
+
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
 end
