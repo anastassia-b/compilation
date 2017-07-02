513c775ac3947177d02fe457a9d9848cc5181098
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-07-02 09:50:41 -0700

    Initial commit

---
+++ 20170628/Intro_JavaScript/node_tic_tac_toe/moveError.js
@@ -0,0 +1,5 @@
+// MoveError really should be a child class of the built in Error object provided
+// by Javascript, but since we haven't covered inheritance yet, we'll just
+// let it be a vanilla Object for now!
+const MoveError = function (msg) { this.msg = msg; };
+module.exports = MoveError;
