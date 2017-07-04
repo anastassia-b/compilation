0e88de3751d7d0a474c4644ae0edcfe015a59ee8
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-07-03 22:57:56 -0700

    Construct view UI

--- 20170703/jQuery_TicTacToe/js/main.js
+++ 20170703/jQuery_TicTacToe/js/main.js
@@ -2,5 +2,7 @@ const View = require('./ttt-view');
 const Game = require('../node_ttt/solution/game');
 
 $( () => {
-
+  const boardEl = $('.ttt');
+  const game = new Game();
+  new View(game, boardEl);
 });
