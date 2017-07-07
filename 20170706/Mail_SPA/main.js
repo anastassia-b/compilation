b1e42df1c210b2041b8c329f37f62edfbaf70ca8
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-07-07 12:54:34 -0700

    Add click event to sidebar-nav items

---
+++ 20170706/Mail_SPA/main.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // let content = document.querySelector(".content");
+  let navItems = Array.from(document.querySelectorAll(".sidebar-nav li"));
+  navItems.forEach(navItem => {
+    navItem.addEventListener("click", () => {
+      let name = navItem.innerText.toLowerCase();
+      location.hash = name;
+    });
+  });
+});
