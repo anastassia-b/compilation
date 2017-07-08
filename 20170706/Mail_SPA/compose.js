8306ff9e7899d94dc03a278ca567715ac46b878d
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-07-08 13:01:01 -0700

    Add ability to compose and send messages

--- 20170706/Mail_SPA/compose.js
+++ 20170706/Mail_SPA/compose.js
@@ -4,7 +4,26 @@ module.exports = {
   renderForm() {
     let currentMessage = MessageStore.getMessageDraft();
     let html = `
+    <p class = "new-message-header">New Message</p>
+    <form class="compose-form">
+    <input
+      placeholder='Recipient'
+      name='to'
+      type="text"
+      value='${currentMessage.to}'>
 
+    <input
+      placeholder='Subject'
+      name='subject'
+      type="text"
+      value='${currentMessage.subject}'>
+
+    <textarea
+      name='body'
+      rows='20'>${currentMessage.body}</textarea>
+
+    <button type="submit" class="btn btn-primary submit-message">Send</button>
+    </form>
     `;
     return html;
   },
@@ -13,6 +32,17 @@ module.exports = {
     container.className = "new-message";
     container.innerHTML = this.renderForm();
 
+    container.addEventListener('change', e => {
+      let target = e.target;
+      MessageStore.updateDraftField(target.name, target.value);
+    });
+
+    container.addEventListener('submit', e => {
+      e.preventDefault();
+      MessageStore.sendDraft();
+      location.hash = "inbox";
+    });
+    
     return container;
   }
 };
