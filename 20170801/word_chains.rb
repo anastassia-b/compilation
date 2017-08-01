dcbd1e3c2924c18fe0db7d825ec5f7c46df4c872
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-08-01 16:57:18 -0700

    Find adjacent words

--- 20170801/word_chains.rb
+++ 20170801/word_chains.rb
@@ -8,6 +8,33 @@ class WordChainer
    @dictionary = Set.new(@dictionary)
   end
 
+  def run(source, target)
+
+  end
+
+  def adjacent_words(word)
+    adj_words = []
+    word.each_char.with_index do |old_letter, i|
+      ('a'..'z').each do |new_letter|
+        next if old_letter == new_letter
+
+        new_word = word.dup
+        new_word[i] = new_letter
+
+        adj_words << new_word if dictionary.include?(new_word)
+      end
+    end
+
+    adj_words
+  end
+
+  def explore_current_words
+
+  end
+
+  def build_path(target)
+
+  end
 end
 
 
