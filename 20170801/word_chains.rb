235d2c8ca79b1a9bc47e8d725355a53d901f4b47
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-08-01 16:37:13 -0700

    Add dictionary

--- 20170801/word_chains.rb
+++ 20170801/word_chains.rb
@@ -1,8 +1,17 @@
 require 'set'
 
 class WordChainer
+ attr_reader :dictionary
 
+ def initialize(dictionary_file_name)
+   @dictionary = File.readlines(dictionary_file_name).map(&:chomp)
+   @dictionary = Set.new(@dictionary)
+  end
 
 end
 
-#testing git authorship
+
+if __FILE__ == $PROGRAM_NAME
+  # p WordChainer.new(ARGV.shift).run("duck", "ruby")
+  p WordChainer.new(ARGV.shift)
+end
