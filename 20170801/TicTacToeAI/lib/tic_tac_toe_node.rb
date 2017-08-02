4a67444bc80fd281da595f0809764fa3797993db
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-08-01 22:41:02 -0700

    Add recursive node status check

--- 20170801/TicTacToeAI/lib/tic_tac_toe_node.rb
+++ 20170801/TicTacToeAI/lib/tic_tac_toe_node.rb
@@ -9,13 +9,44 @@ class TicTacToeNode
   end
 
   def losing_node?(evaluator)
+    if board.over?
+      return board.won? && board.winner != evaluator
+    end
+
+    if self.next_mover_mark == evaluator
+      self.children.all? { |node| node.losing_node?(evaluator) }
+    else
+      self.children.any? { |node| node.losing_node?(evaluator) }
+    end
   end
 
   def winning_node?(evaluator)
+    if board.over?
+      return board.winner == evaluator
+    end
+
+    if self.next_mover_mark == evaluator
+      self.children.any? { |node| node.winning_node?(evaluator) }
+    else
+      self.children.all? { |node| node.winning_node?(evaluator) }
+    end
   end
 
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    children = []
+
+    (0..2).each do |row_idx|
+      (0..2).each do |col_idx|
+        pos = [row_idx, col_idx]
+
+        next unless board.empty?(pos)
+
+        new_board = board.dup
+        new_board[pos] = self.next_mover_mark
+        next_mover_mark = (self.next_mover_mark == :x ? :o : :x)
+
+        children << TicTacToeNode.new(new_board, next_mover_mark, pos)
   end
 end
