f8b00e42f37083d189bad82ac9e330bd3ed7086b
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-08-03 16:06:41 -0700

    Initialize tile

---
+++ 20170803/tile.rb
@@ -0,0 +1,36 @@
+class Tile
+  DELTAS = [
+    [-1, -1],
+    [-1,  0],
+    [-1,  1],
+    [ 0, -1],
+    [ 0,  1],
+    [ 1, -1],
+    [ 1,  0],
+    [ 1,  1]
+  ]
+
+  attr_reader :pos
+
+  def initialize(board, pos)
+    @board, @pos = board, pos
+    @bombed, @explored, @flagged = false, false, false
+  end
+
+  def bombed?
+    @bombed
+  end
+
+  def explored?
+    @explored
+  end
+
+  def flagged?
+    @flagged
+  end
+
+  def adjacent_bomb_count
+    neighbors.select(&:bombed?).count
+  end
+
+end
