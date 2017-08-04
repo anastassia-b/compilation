d034cc28b8cd82f57107f71722487e95cc1ade73
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-08-03 17:23:32 -0700

    Add tile methods

--- 20170803/tile.rb
+++ 20170803/tile.rb
@@ -34,31 +34,62 @@ class Tile
   end
 
   def explore
+    return self if flagged?
+    return self if explored?
 
+    @explored = true
+    if !bombed? && adjacent_bomb_count == 0
+      neighbores.each { |adj_tile| adj_tile.explore }
+    end
+
+    self
   end
 
   def inspect
-
+    { :pos => pos,
+      :bombed => bombed?,
+      :flagged => flagged?,
+      :explored => explored? }.inspect
   end
 
   def neighbors
+    adjacent_coords = DELTAS.map do |(dx, dy)|
+      [pos[0] + dx, pos[1] + dy]
+    end.select do |row, col|
+      [row, col].all? do |coord|
+        coord.between?(0, @board.grid_size - 1)
+      end
+    end
 
+    adjacent_coords.map { |pos| @board[pos] }
   end
 
   def plant_bomb
-
+    @bombed = true
   end
 
   def render
-
+    if flagged?
+      "F"
+    elsif explored?
+      adjacent_bomb_count == 0 ? "_" : adjacent_bomb_count.to_s
+    else
+      "*"
+    end
   end
 
   def reveal
-
+    if flagged?
+      bombed? ? "F" : "f"
+    elsif bombed?
+      explored? ? "X" : "B"
+    else
+      adjacent_bomb_count == 0 ? "_" : adjacent_bomb_count.to_s
+    end
   end
 
   def toggle_flag
-
+    @flagged = !@flagged unless @explored
   end
 
 end
