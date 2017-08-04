2502035a623216b0d4159324edfc561b923c4753
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-08-03 22:04:16 -0700

    Generate board with bombs

--- 20170803/board.rb
+++ 20170803/board.rb
@@ -17,24 +17,41 @@ class Board
   end
 
   def render(reveal = false)
-
+    @grid.map do |row|
+      row.map do |tile|
+        reveal ? tile.reveal : tile.render
+      end.join("")
+    end.join("\n")
   end
 
   def reveal
-
+    render(true)
   end
 
   def won?
-
+    @grid.flatten.all? { |tile| tile.bombed? != tile.explored? }
   end
 
   private
 
   def generate_board
-
+    @grid = Array.new(@grid_size) do |row|
+      Array.new(@grid_size) { |col| Tile.new(self, [row, col]) }
+    end
+    place_bombs
   end
 
-  def plant_bombs
+  def place_bombs
+    total_bombs = 0
+    while total_bombds < @num_bombs]
+      rand_pos = Array.new(2) { rand(@grid_size) }
+      tile = self[rand_pos]
+      next if tile.bombed?
 
+      tile.plant_bomb
+      total_bombs += 1
+    end
+
+    nil
   end
 end
