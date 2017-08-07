eb96639fd3b6c277e546086b669ec33546948759
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-08-06 20:59:47 -0700

    Fetch display data

--- 20170804/NativeApp/App.js
+++ 20170804/NativeApp/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, TextInput, Button, Alert, ScrollView, FlatList, SectionList } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, Button, Alert,
+        ScrollView, FlatList, SectionList, ActivityIndicator, ListView } from 'react-native';
 
 export default class App extends React.Component {
   render() {
@@ -17,11 +18,57 @@ export default class App extends React.Component {
     //     <TextInANest/>
     //     <ButtonBasics/>
     //     <Scrolling/>
+    //     <BasicSectionList>
     //   </View>
     // );
 
     return (
-      <BasicSectionList/>
+      <Movies/>
+    );
+  }
+}
+
+class Movies extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true
+    }
+  }
+
+  componentDidMount() {
+    return fetch('https://facebook.github.io/react-native/movies.json')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        this.setState({
+          isLoading: false,
+          dataSource: ds.cloneWithRows(responseJson.movies)
+        }, function() {
+          //do something with this new state
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return (
+        <View style={{flex: 1, paddingTop: 20}}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
+
+    return (
+      <View style={{flex: 1, paddingTop: 20}}>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={(rowData) => <Text>{rowData.title}, {rowData.releaseYear}</Text>}
+        />
+      </View>
     );
   }
 }
