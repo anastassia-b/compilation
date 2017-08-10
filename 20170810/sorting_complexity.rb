108a63576df384a8450a53190f1dc758855a5944
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-08-10 10:02:55 -0700

    Add benchmark performance tests

--- 20170810/sorting_complexity.rb
+++ 20170810/sorting_complexity.rb
@@ -1,3 +1,5 @@
+require 'io/console'
+require 'benchmark'
 
 class SortingDemo
 
@@ -55,5 +57,49 @@ class SortingDemo
     merged_array + left + right
   end
 
+  def self.performance_test(size, count)
+    arrays_to_test = Array.new(count) { random_arr(size) }
 
+    Benchmark.benchmark(Benchmark::CAPTION, 9, Benchmark::FORMAT,
+                        "Avg. Merge: ", "Avg. Bubble: ") do |b|
+      merge = b.report("Tot. Merge: ") { run_merge_sort(arrays_to_test) }
+      bubble = b.report("Tot. Bubble: ") { run_bubble_sort(arrays_to_test) }
+      [merge/count, bubble/count]
+    end
+  end
+
+  def self.random_arr(n)
+    Array.new(n) { rand(n) }
+  end
+
+  def self.run_bubble_sort(arrays)
+    arrays.each do |array|
+      array_to_test = array.dup
+      bubble_sort(array_to_test)
+    end
+  end
+
+  def self.run_merge_sort(arrays)
+    arrays.each do |array|
+      array_to_test = array.dup
+      merge_sort(array_to_test)
+    end
+  end
+
+  def self.run_performance_tests(multiplier = 5, rounds = 3)
+    [1, 10, 100, 1000, 10000].each do |size|
+      size *= multiplier
+      wait_for_keypress(
+        "\nPress any key to benchmark sorts for #{size} elements"
+      )
+      performance_test(size, rounds)
+    end
+  end
+
+  def self.wait_for_keypress(prompt)
+    puts prompt
+    STDIN.getch
+  end
 end
+
+SortingDemo.run_performance_tests
