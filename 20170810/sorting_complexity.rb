59d379e6adcd9a6817987133b80b55533f06fc07
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-08-10 09:53:26 -0700

    Add merge sort

--- 20170810/sorting_complexity.rb
+++ 20170810/sorting_complexity.rb
@@ -24,6 +24,36 @@ class SortingDemo
     array
   end
 
+  #Merge sort is O(nlogn)
+
+  def self.merge_sort(array, &prc)
+    return array if array.length <= 1
+
+    mid = array.length / 2
+    merge(
+      merge_sort(array.take(mid), &prc),
+      merge_sort(array.drop(mid), &prc),
+      &prc
+    )
+  end
+
+  def self.merge(left, right, &prc)
+    prc ||= Proc.new { |num1, num2| num1 <=> num2 }
+    merged_array = []
+
+    until left.empty? || right.empty?
+      case prc.call(left.first, right.first)
+      when -1
+        merged_array << left.shift
+      when 0
+        merged_array << left.shift
+      when 1
+        merged_array << right.shift
+      end
+    end
+
+    merged_array + left + right
+  end
 
 
 end
