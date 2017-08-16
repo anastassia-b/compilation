9459d8c11bc4fd7b98234d67cecb5b61f568a12f
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-08-15 23:13:30 -0700

    Refactor, add model base class

--- 20170613/model_base.rb
+++ 20170613/model_base.rb
@@ -1,2 +1,37 @@
 require 'active_support/inflector'
 require_relative 'questions_db'
+
+class ModelBase
+  def self.table
+    self.to_s.tableize
+  end
+
+  def self.find_by_id(id)
+    data = QuestionsDatabase.get_first_row(<<-SQL, id: id)
+      SELECT
+        *
+      FROM
+        #{table}
+      WHERE
+        id = :id
+    SQL
+
+    data.nil? ? nil : self.new(data)
+  end
+
+  def self.all
+    data = QuestionsDatabase.execute(<<-SQL)
+      SELECT
+        *
+      FROM
+        #{table}
+    SQL
+
+    parse_all(data)
+  end
+
+  def self.parse_all(data)
+    data.map { |attrs| self.new(attrs) }
+  end
+
+end
