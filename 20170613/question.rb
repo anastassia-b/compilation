ac62bebf3cf71a1402fe9cadfc074d4a3f9b8509
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-08-15 23:31:21 -0700

    Save questions to database

--- 20170613/question.rb
+++ 20170613/question.rb
@@ -2,12 +2,14 @@ require_relative 'questions_db'
 require_relative 'model_base'
 
 class Question < ModelBase
-  attr_accessor :title, :body, :author
+  attr_reader :id
+  attr_accessor :title, :body, :author_id
 
   def initialize(options)
+    @id = options['id']
     @title = options['title']
     @body = options['body']
-    @author = options['author']
+    @author_id = options['author_id']
   end
 
   def self.all
@@ -22,14 +24,54 @@ class Question < ModelBase
       FROM
         questions
       WHERE
-        id = ?
+        questions.id = ?
     SQL
-    return nil unless question.length > 0
 
-    Question.new(question.first)
+    question.nil? ? nil : Question.new(question.first)
   end
 
   def self.find_by_author_id(author_id)
+    question = QuestionsDatabase.instance.execute(<<-SQL, author_id)
+      SELECT
+        *
+      FROM
+        questions
+      WHERE
+        questions.author_id = ?
+    SQL
 
+    question.nil? ? nil : question.map { |q| Question.new(q) }
+  end
+
+  def attrs
+    { title: title, body: body, author_id: author_id }
+  end
+
+  def save
+    if @id
+      QuestionsDatabase.execute(<<-SQL, attrs.merge({ id: id }))
+        UPDATE
+          questions
+        SET
+          title = :title, body = :body, author_id = :author_id
+        WHERE
+          questions.id = :id
+      SQL
+    else
+      QuestionsDatabase.execute(<<-SQL, attrs)
+        INSERT INTO
+          questions (title, body, author_id)
+        VALUES
+          (:title, :body, :author_id)
+      SQL
+
+      @id = QuestionsDatabase.last_insert_row_id
+    end
+
+    self
+  end
+
+  def author
+    User.find_by_id(author_id)
   end
 end
