81b9e8cb12f485bb4af571720fa8ed117d17abaa
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-08-16 22:37:08 -0700

    Add visit model validations, associations

--- 20170816/URLShortener/app/models/visit.rb
+++ 20170816/URLShortener/app/models/visit.rb
@@ -1,2 +1,14 @@
 class Visit < ApplicationRecord
+  validates :visitor, :short_url, presence: true
+
+  belongs_to :short_url
+
+  belongs_to :visitor,
+    class_name: :User,
+    foreign_key: :user_id,
+    primary_key: :id
+
+  def self.record_visit!(user, short_url)
+    Visit.create!(user_id: user.id, short_url_id: short_url.id)
+  end
 end
