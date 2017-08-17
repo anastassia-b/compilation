630b30eaae4b9dd3317c132e9afe78c1ff72efaa
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-08-16 22:30:01 -0700

    Add user and shortURL model validations, associations

--- 20170816/URLShortener/app/models/short_url.rb
+++ 20170816/URLShortener/app/models/short_url.rb
@@ -1,2 +1,24 @@
 class ShortUrl < ApplicationRecord
+  validates :long_url, :short_url, :submitter, presence: true
+  validates :short_url, uniqueness: true
+
+  belongs_to :submitter,
+    class_name: :User,
+    foreign_key: :submitter_id,
+    primary_key: :id
+
+  def self.random_code
+    loop do
+      random_code = SecureRandom.urlsafe_base64(16)
+      return random_code unless ShortenedUrl.exists?(short_url: random_code)
+    end
+  end
+
+  def self.create_for_user_and_long_url!(user, long_url)
+    ShortenedUrl.create!(
+      submitter_id: user.id,
+      long_url: long_url,
+      short_url: ShortenedUrl.random_code
+    )
+  end
 end
--- 20170816/URLShortener/app/models/user.rb
+++ 20170816/URLShortener/app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates :email, uniqueness: true, presence: true
+
+  has_many :submitted_urls,
+    class_name: :ShortURL,
+    foreign_key: :submitter_id,
+    primary_key: :id
 end
