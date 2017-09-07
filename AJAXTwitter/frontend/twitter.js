5c58c91cbd155fa1d50fa561ce950ee637bf4cee
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-09-06 17:32:14 -0700

    Add User Search

--- AJAXTwitter/frontend/twitter.js
+++ AJAXTwitter/frontend/twitter.js
@@ -1,5 +1,7 @@
 const FollowToggle = require('./follow_toggle');
+const UserSearch = require('./users_search');
 
 $(() => {
   $('button.follow-toggle').each( (i, btn) => new FollowToggle(btn, {}));
+  $('nav.users-search').each ( (i, search) => new UsersSearch(search));
 }).
---
+++ AJAXTwitter/frontend/users_search.js
@@ -0,0 +1,20 @@
+const APIUtil = require('./api_util');
+const FollowToggle = require('./follow_toggle');
+
+class UserSearch {
+  constructor(el) {
+
+  }
+
+  handleInput(event) {
+
+  }
+
+  renderResults(users) {
+
+  }
+
+
+}
+
+module.exports = UsersSearch;
