1a43d7c990dbeaf8a2ba65b47dfb36e8862df356
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-10-11 12:46:43 -0700

    Add initialization tests

--- 20171011/hackathon.rb
+++ 20171011/hackathon.rb
@@ -1,10 +1,10 @@
 require 'rspec'
 
 class Hackathon
-  attr_reader :num_attendees
-  def initialize(capacity)
+  attr_reader :attendees, :theme
+  def initialize(theme, capacity)
+    @theme = theme
     @capacity = capacity
-    @num_attendees = 0
     @attendees = []
   end
 end
@@ -18,5 +18,22 @@ end
 class Attendee < Person
   def initialize(name)
     super(name)
+  end
+end
+
+describe Hackathon do
+  # subject { Hackathon.new(10) }
+  subject(:hackathon) { Hackathon.new("Testing", 10) }
+  # let(:hackathon) { Hackathon.new(10) }
+
+  context "upon initialization" do
+    it "should start with 0 attendees" do
+      expect(hackathon.attendees).to be_empty
+    end
+
+    it "should have a theme" do
+      expect(hackathon.theme).to be_a(String)
+    end
+
   end
 end
