fdecdc450ef4de2233bf15a05ae5f9f0e19bca20
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-10-20 23:25:24 -0700

    Add conference contract

--- Ethereum/conference_example/contract.sol
+++ Ethereum/conference_example/contract.sol
@@ -1,3 +1,49 @@
 contract Conference {
-  
+  address public organizer;
+  mapping (address => uint) public registrantsPaid;
+  uint public numRegistrants;
+  uint public quota;
+
+  event Deposit(address _from, uint _amount);
+  event Refund(address _to, uint _amount);
+
+  //Constructor
+  function Conference() {
+    organizer = msg.sender;
+    quota = 500;
+    numRegistrants = 0;
+  }
+
+  function buyTicket() public returns (bool success) {
+    if (numRegistrants >= quota) { return false; }
+
+    registrantsPaid[msg.sender] = msg.value;
+    numRegistrants++;
+    Deposit(msg.sender, msg.value);
+    return true;
+  }
+
+  function changeQuota(uint newquota) public {
+    if (msg.sender != organizer) { return; }
+    quota = newquota;
+  }
+
+  function refundTicket(address recipient, uint amount) public {
+    if (msg.sender != organizer) { return; }
+    if (registrantsPaid[recipient] == amount) {
+      address myAddress = this;
+      if (myAddress.balance >= amount) {
+        recipient.send(amount);
+        registrantsPaid[recipient] = 0;
+        numRegistrants--;
+        Refund(recipient, amount);
+      }
+    }
+  }
+
+  function destroy() {
+    if (msg.sender == organizer) {
+      suicide(organizer);
+    }
+  }
 }
