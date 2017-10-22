634bb5d813fae2ae1739183b15f44b0b81404976
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-10-21 19:10:42 -0700

    Add basic storage

---
+++ Ethereum/hackathon_notes/smart_contract.sol
@@ -0,0 +1,13 @@
+pragms solidity ^0.4.0;
+
+contract SimpleStorage {
+  uint storedData;
+
+  function set(uint x) {
+    storedData = x;
+  }
+
+  function get() constant returns (uint) {
+    return storedData;
+  }
+}
